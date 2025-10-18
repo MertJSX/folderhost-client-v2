fa4557700d85e2172ab5e4cbbe9a8fe049cdc3c3
MertJSX <mertJSX@proton.me>
2025-10-18 18:32:18 +0300

    chore: simplify

--- src/pages/HomePage/Home.tsx
+++ src/pages/HomePage/Home.tsx
@@ -1,37 +1,21 @@
-import { Link } from 'react-router-dom'
-import { FaHome } from "react-icons/fa";
+import { useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie';
 import React from 'react';
 
 const Home: React.FC = () => {
+  const navigate = useNavigate();
+
+  React.useEffect(() => {
+    if (Cookies.get("token")) {
+      navigate(`/explorer/${encodeURIComponent("./")}`)
+    } else {
+      navigate("/login")
+    }
+  }, [])
+
   return (
-    <div>
-        <div className="flex flex-col justify-center items-center bg-gray-800 w-1/2 mx-auto rounded-lg p-5 gap-4 mt-20">
-                <FaHome size={65} />
-                {
-                  !Cookies.get("token") ?
-                  (
-                    <h1 className='text-2xl text-center'>Please login to get access!</h1>
-                  ) : (
-                    <h1 className='text-2xl text-center'>Welcome!</h1>
-                  )
-                }
-                
-                {
-                  !Cookies.get("token") ? (
-                    <div className='w-full flex flex-col'>
-                      <Link className='text-center font-bold text-2xl bg-sky-700 w-1/2 mx-auto rounded-xl m-4 border-2 border-sky-500' to="/login">Login</Link>
-                    </div>
-                  ) : (
-                    <div className='w-full flex flex-col'>
-                      <Link 
-                      className='transition-all text-center font-bold text-2xl bg-sky-700 hover:bg-sky-600 w-1/2 mx-auto rounded-xl m-4' 
-                      to={`/explorer/${encodeURIComponent("./")}`}>Explorer</Link>
-                    </div>
-                  )
-                }
-          </div>
-    </div>
+    <>
+    </>
   )
 }
 
